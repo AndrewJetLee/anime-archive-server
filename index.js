20b05b4044289a859aa9ef7baf9e0f90778a57da
AndrewJetLee <andrewjetlee777@gmail.com>
2022-04-02 16:40:23 -0500

    Testing proxy reversion

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ const passport = require("passport");
 // initialize server
 const app = express();
 const PORT = process.env.PORT || 4000;
-const CLIENT_ORIGIN = process.env.PROD_ORIGIN || "http://localhost:3000"
+const CLIENT_ORIGIN = process.env.PROD_ORIGIN || "http://localhost:3000";
 const environment = process.env.NODE_ENV;
 
 // mount middleware
-app.use(cors({credentials: true, origin: CLIENT_ORIGIN}));
+app.use(cors({ credentials: true, origin: CLIENT_ORIGIN }));
 app.use(express.json());
 app.use(
   express.urlencoded({
@@ -22,22 +22,26 @@ app.use(
 );
 
 // session setup
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  store: MongoStore.create({ mongoUrl: process.env.DB_STRING}),
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    maxAge: 1000 * 60 * 60 * 24,
-    sameSite: environment === "production" ? 'none' : "lax",
-    secure: environment === "production" ? true : false
-  }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING }),
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24,
+      sameSite: environment === "production" ? "none" : "lax",
+      secure: environment === "production" ? true : false,
+    },
+  })
+);
 
-// app.set('trust proxy', 1);
+if (environment === "production") {
+  app.set("trust proxy", 1);
+}
 
-// passport 
-require('./config/passport');
+// passport
+require("./config/passport");
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -54,19 +58,19 @@ app.get("/", async (req, res) => {
   } catch (err) {
     res.send(err);
   }
-})
+});
 
 // assign routers
 app.use("/api/user", userRouter);
 
 // connect to db
-connectToDb()
+connectToDb();
 
 // start server
 const startServer = async () => {
   try {
     await app.listen(PORT);
-    console.log(`Successfully started server on port:${PORT}`)
+    console.log(`Successfully started server on port:${PORT}`);
   } catch (err) {
     console.log("Error starting server", err);
   }
